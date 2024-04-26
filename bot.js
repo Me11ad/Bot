6115d37d421af375197498821b460b05bd08fc6f
Vlad Melnichenko <vlad505200483@gmail.com>
2024-04-26 16:13:56 +0500

    Its working

--- bot.js
+++ bot.js
@@ -3,17 +3,12 @@ const { Telegraf } = require('telegraf');
 const axios = require('axios')
 
 const bot = new Telegraf("7029601267:AAHByV5mCYf1Hpqr5Wh_nxq3m9L1H3r36bM");
-bot.start((ctx) => ctx.reply('Отправьте свою геолокацию для занния погоды'));
+bot.start((ctx) => ctx.reply('Отправьте свою геолокацию для знания погоды'));
 
-
-
-// // Enable graceful stop
-// process.once('SIGINT', () => bot.stop('SIGINT'));
-// process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 bot.on ('message',async (ctx) => {
     if(ctx.message.location){
-        const weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=b3e8e66d34e5a4fe0ed12782ec604b30`
+        const weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&lang=ru&appid=b3e8e66d34e5a4fe0ed12782ec604b30&units=metric`
         const response = await axios.get(weatherAPIUrl);
         ctx.reply(`${response.data.name}: ${response.data.weather[0].main} ${response.data.main.temp} °C`);
     }
